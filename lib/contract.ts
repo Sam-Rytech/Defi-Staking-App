674bc19f22577af5b6c8f4d3545929db0c6580ec
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-05 02:18:41 +0100

    Added BuilderToken.json from remix

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,5 +1,5 @@
-import stakingAbi from './abis/StakingContract.json'
-import tokenAbi from './abis/BuilderToken.json'
+import stakingAbi from "../abis/StakingContract.json"
+import tokenAbi from '../abis/BuilderToken.json'
 
 export const STAKING_CONTRACT_ADDRESS = '0xYourStakingContractAddress'
 export const TOKEN_CONTRACT_ADDRESS = '0xYourTokenContractAddress'
