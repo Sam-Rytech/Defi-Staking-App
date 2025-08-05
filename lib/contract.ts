d2a4530d5dcd65d9d4ec2d82543277c12cacffb9
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-05 02:14:56 +0100

    Added the contract abi

---
+++ lib/contract.ts
@@ -0,0 +1,8 @@
+import stakingAbi from './abis/StakingContract.json'
+import tokenAbi from './abis/BuilderToken.json'
+
+export const STAKING_CONTRACT_ADDRESS = '0xYourStakingContractAddress'
+export const TOKEN_CONTRACT_ADDRESS = '0xYourTokenContractAddress'
+
+export const STAKING_ABI = stakingAbi
+export const TOKEN_ABI = tokenAbi
