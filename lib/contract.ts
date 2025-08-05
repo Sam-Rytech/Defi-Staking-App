89a12a183f233b40d9b759eb3adf9ca78da936a7
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-05 02:34:54 +0100

    Fixed run time error

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,8 +1,21 @@
+// lib/contract.ts
+import { ethers } from 'ethers'
 import stakingAbi from "../abis/StakingContract.json"
 import tokenAbi from '../abis/BuilderToken.json'
 
 export const STAKING_CONTRACT_ADDRESS = '0x94d494e4c1e6830dc3889d4f188dbd7bb6a32d03'
 export const TOKEN_CONTRACT_ADDRESS = '0x8D4A4C15651b51C46B9566FBcF109B0eF090F1b2'
 
-export const STAKING_ABI = stakingAbi
-export const TOKEN_ABI = tokenAbi
+export const STAKING_ABI = stakingAbi.abi
+export const TOKEN_ABI = tokenAbi.abi
+
+export const getProvider = () => {
+  if (!window.ethereum) throw new Error('No wallet found')
+  return new ethers.BrowserProvider(window.ethereum)
+}
+
+export const getSigner = async () => {
+  const provider = getProvider()
+  return await provider.getSigner()
+}
+
