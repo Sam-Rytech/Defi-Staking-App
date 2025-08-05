de73923671442970b5d404217505049f350b9544
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-05 02:20:51 +0100

    Updated contract address

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,8 +1,8 @@
 import stakingAbi from "../abis/StakingContract.json"
 import tokenAbi from '../abis/BuilderToken.json'
 
-export const STAKING_CONTRACT_ADDRESS = '0xYourStakingContractAddress'
-export const TOKEN_CONTRACT_ADDRESS = '0xYourTokenContractAddress'
+export const STAKING_CONTRACT_ADDRESS = '0x94d494e4c1e6830dc3889d4f188dbd7bb6a32d03'
+export const TOKEN_CONTRACT_ADDRESS = '0x8D4A4C15651b51C46B9566FBcF109B0eF090F1b2'
 
 export const STAKING_ABI = stakingAbi
 export const TOKEN_ABI = tokenAbi
